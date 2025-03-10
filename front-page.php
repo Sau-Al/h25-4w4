600b8f8b374f32a9abf6f656462b1fa221198745
Alicia <e2249485@cmaisonneuve.qc.ca>
2025-03-10 14:20:47 -0400

    ajout des customizers

--- front-page.php
+++ front-page.php
@@ -1,5 +1,9 @@
     <?php get_header(); ?>
-    <section class="hero">
+    <?php 
+    $hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+    $hero_background = get_theme_mod('hero_background', ''); 
+    ?>
+    <section class="hero" style="background-image: url(<?php echo $hero_background?>)">
         <div class="hero__contenu  global">
             <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
             <p class="hero__description"><?php bloginfo('description'); ?>
@@ -9,6 +13,7 @@
             </p>
             <p class="hero__adresse">5800 Sherbrooke-est - Montréal (Québec) h1X 2A2</p>
             <p class="hero__numero">514-254-7131</p>
+            <p class="hero_auteur">Auteur: <?php echo $hero_auteur?></p>
             <button class="hero__button">
                 S'inscrire
             </button>
@@ -60,7 +65,7 @@
             ?>
             <div class="cartes">
                 <!-- Les cartes -->
-                <?php get_template_part("gabarit/carte");?>
+                <?php get_template_part("gabarits/carte");?>
                 <?php } ?>
                 <?php endwhile; endif; ?>
             </div>
