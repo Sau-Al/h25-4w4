33c6422fcb4da5b9283767355039c4b69836b443
Alicia Sau <e2249485@cmaisonneuve.qc.ca>
2025-03-31 13:57:09 -0400

    s9c1 Intra Version mobile fini

--- 404.php
+++ 404.php
@@ -5,19 +5,22 @@
 <?php get_header(); ?>
 <?php $error_background = get_theme_mod('error_background', ''); ?>
     <section class="section_404" style="background-image: url('<?php echo esc_url(get_theme_mod('error_background')); ?>');">
-        <div class="texte_erreur">
-            <div class="titre_404">
-                <h1>Oops, vous avez échoué sur l'île 404 !</h1>
-            </div>
-            <div class="para_404"><p>Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !</p></div>
-            <button class="bouton_pg_error">Retour à la page d'accueil</button>
-        </div>
-        <!-- Menu -->
-        <?php wp_nav_menu(array(
+       <div class="erreur__global">
+            <div class="texte_erreur">
+                <div class="titre_404">
+                    <h1>Oops, vous avez échoué sur l'île 404 !</h1>
+                </div>
+                <div class="para_404">
+                    <p>Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !</p></div>
+                    <button class="bouton_pg_error">Retour à la page d'accueil</button>
+                </div>
+            <!-- Menu -->
+            <?php wp_nav_menu(array(
                 "menu" => "404",
                 "container" => "nav",
                 "menu_class" => "erreur__menu"
             )); ?>
+        </div>
     </section>
     <?php get_footer(); ?>
    
