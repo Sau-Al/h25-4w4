6fb3351f65d67e8bf96cc40bec4dd54963cfb145
Alicia <e2249485@cmaisonneuve.qc.ca>
2025-05-05 14:12:10 -0400

    Texte de la zone Hero animation

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,14 +1,15 @@
 (function () {
     console.log("carrousel.js");
- 
+
     const radios = document.querySelectorAll('.hero__radio__input');
     const slides = document.querySelectorAll('.hero__carrousel');
+    const contenu = document.querySelector('.hero__contenu'); // <-- ajout ici
     let currentIndex = 0;
     let intervalID;
- 
+
     console.log("Nombre d'inputs =", radios.length);
     console.log("Nombre de slides =", slides.length);
- 
+
     // Fonction pour afficher uniquement le slide actif
     function afficherSlide(index) {
         slides.forEach((slide, i) => {
@@ -16,11 +17,18 @@
             radios[i].checked = (i === index); // synchronise le bouton radio
         });
         currentIndex = index;
+
+        // 🔁 Animation du contenu texte
+        if (contenu) {
+            contenu.classList.remove('animated');
+            void contenu.offsetWidth; // force le reflow pour relancer l'animation
+            contenu.classList.add('animated');
+        }
     }
- 
+
     // Initialisation : afficher le premier slide
     afficherSlide(0);
- 
+
     // Ajouter un écouteur à chaque bouton radio
     radios.forEach((radio, index) => {
         radio.addEventListener('change', function () {
@@ -28,25 +36,25 @@
             restartInterval(); // recommencer l’intervalle après interaction
         });
     });
- 
+
     // Fonction pour passer au slide suivant automatiquement
     function slideSuivant() {
         const prochainIndex = (currentIndex + 1) % slides.length;
         afficherSlide(prochainIndex);
     }
- 
+
     // Démarre l'animation automatique
     function startInterval() {
         intervalID = setInterval(() => {
             slideSuivant();
         }, 5000);
     }
- 
+
     function restartInterval() {
         clearInterval(intervalID);
         startInterval();
     }
- 
+
     // Lancer l’animation au démarrage
     startInterval();
 })();
