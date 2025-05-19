38dd75a4ea93c626df3d5d441b1643a21542dd6f
Alicia <e2249485@cmaisonneuve.qc.ca>
2025-05-18 20:56:01 -0400

    Changement scss 404

--- 404.php
+++ 404.php
@@ -1,33 +1,42 @@
-<!-- Customizers -->
-<!-- Couleur, image arriere plan, titre, texte -->
- <!-- Creer un sass pour la page 404 -->
-<?php
-    $erreur_text = get_theme_mod('erreur_text', 'OOPS');
-    $erreur_parag = get_theme_mod('erreur_parag', '');
-    $erreur_cta_text = get_theme_mod('erreur_cta_text', 'Default CTA'); 
-    $error_background = get_theme_mod('error_background', '');
-    $main_color = get_theme_mod('main_color', '');
-?> 
-
-
 <?php get_header(); ?>
-    <section class="section_404" style="background-image: url('<?php echo esc_url(get_theme_mod('error_background')); ?>');">
-       <div class="erreur__global">
-            <div class="texte_erreur">
-                <div class="titre_404">
-                    <h1><?php echo esc_html($erreur_text);?></h1>
-                </div>
-                <div class="para_404">
-                    <p><?php echo esc_html($erreur_parag); ?></p></div>
-                    <button class="bouton_pg_error"><?php echo esc_html($erreur_cta_text); ?></button>
-                </div>
-            <!-- Menu -->
-            <?php wp_nav_menu(array(
-                "menu" => "404",
-                "container" => "nav",
-                "menu_class" => "erreur__menu"
-            )); ?>
+<?php $error_background = get_theme_mod('error_background', ''); ?>
+    <section class="error" style="background-image: url('<?php echo esc_url(get_theme_mod('error_background')); ?>');">
+        <div class="texte_erreur">
+            <h1>Désolé!</h1>
+            <h3>La page que vous recherchez ne peut pas être trouvée.</h3>
+            <p>Les raisons possibles à ce problème:</p>
+            <ul>
+                <li>
+                    L'adresse n'est pas bien écrite.
+                </li>
+                <li>
+                    Le lien est brisé ou trop vieux.
+                </li>
+            </ul>
+            <button class="bouton_pg_error">Retour à la page d'accueil</button>
+            <button class="bouton_pg_error">Aide</button>
         </div>
+        <div class="recherche_erreur">
+        <div class="texte_erreur_recherche">
+            <h1>Rechercher une nouvelle destination!</h1>
+            <h3>Découvrez plusieurs endroits formidables pour vos prochains vacances</h3>
+            </div>
+            <?php get_search_form(); ?>
+            <p>Veuiller visiter nos réseaux sociaux pour toute autres infos</p>
+            <!-- Réseaux sociaux -->
+            <div class="error__social">
+            <a href="#" class="social-icon facebook">
+                <img src="https://s2.svgbox.net/materialui.svg?ic=facebook" alt="Facebook">
+            </a>
+            <a href="#" class="social-icon linkedin">
+                <img src="https://s2.svgbox.net/social.svg?ic=linkedin" alt="LinkedIn">
+            </a>
+            <a href="#" class="social-icon discord">
+                <img src="https://s2.svgbox.net/social.svg?ic=discord" alt="Discord">
+            </a>
+            </div>
+        </div>
+        
     </section>
     <?php get_footer(); ?>
    
---
+++ 404_examen.php
@@ -0,0 +1,35 @@
+<!-- Customizers -->
+<!-- Couleur, image arriere plan, titre, texte -->
+ <!-- Creer un sass pour la page 404 -->
+<?php
+    $erreur_text = get_theme_mod('erreur_text', 'OOPS');
+    $erreur_parag = get_theme_mod('erreur_parag', '');
+    $erreur_cta_text = get_theme_mod('erreur_cta_text', 'Default CTA'); 
+    $error_background = get_theme_mod('error_background', '');
+    $main_color = get_theme_mod('main_color', '');
+?> 
+
+
+<?php get_header(); ?>
+    <section class="section_404" style="background-image: url('<?php echo esc_url(get_theme_mod('error_background')); ?>');">
+       <div class="erreur__global">
+            <div class="texte_erreur">
+                <div class="titre_404">
+                    <h1><?php echo esc_html($erreur_text);?></h1>
+                </div>
+                <div class="para_404">
+                    <p><?php echo esc_html($erreur_parag); ?></p></div>
+                    <button class="bouton_pg_error"><?php echo esc_html($erreur_cta_text); ?></button>
+                </div>
+            <!-- Menu -->
+            <?php wp_nav_menu(array(
+                "menu" => "404",
+                "container" => "nav",
+                "menu_class" => "erreur__menu"
+            )); ?>
+        </div>
+    </section>
+    <?php get_footer(); ?>
+   
+</body>
+</html>
